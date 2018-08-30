b71537bf63f73f1f118b73614ca41649ce933c12
Nao YONASHIRO <owan.orisano@gmail.com>
2018-08-31 01:36:20 +0900

    feat: use copied flag

--- ordered.go
+++ ordered.go
@@ -10,7 +10,8 @@ func Ordered(names ...string) func() {
 	return func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		for _, name := range names {
-			f := flag.Lookup(name)
+			f := *flag.Lookup(name)
+			f.Value.Set(f.DefValue)
 			fs := flag.NewFlagSet("", flag.PanicOnError)
 			fs.Var(f.Value, f.Name, f.Usage)
 			fs.PrintDefaults()
